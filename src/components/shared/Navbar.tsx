b6faa9034be11c3c208ce1b6608f7ee56c48e7f5
codewithshamim <shamimislamonline@gmail.com>
2023-07-28 21:35:45 +0600

    categories dropdown added

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -16,32 +16,82 @@ const { Header } = Layout;
 const items: MenuProps["items"] = [
   {
     key: "1",
-    label: <Link href="/cpu-processor">CPU / Processor</Link>,
+    label: (
+      <div className="hover:bg-primary transition-all rounded p-2 w-full">
+        <Link href="/products/cpu" className="text-black hover:text-white">
+          CPU / Processor
+        </Link>
+      </div>
+    ),
   },
   {
     key: "2",
-    label: <Link href="/">Motherboard</Link>,
+    label: (
+      <div className="hover:bg-primary transition-all rounded p-2 w-full">
+        <Link
+          href="/products/motherboard"
+          className="text-black hover:text-white"
+        >
+          Motherboard
+        </Link>
+      </div>
+    ),
   },
   {
     key: "3",
-    label: <Link href="/">RAM</Link>,
+    label: (
+      <div className="hover:bg-primary transition-all rounded p-2 w-full">
+        <Link href="/products/ram" className="text-black hover:text-white">
+          RAM
+        </Link>
+      </div>
+    ),
   },
   {
     key: "4",
-    label: <Link href="/">Power Supply Unit</Link>,
+    label: (
+      <div className="hover:bg-primary transition-all rounded p-2 w-full">
+        <Link
+          href="/products/power-supply"
+          className="text-black hover:text-white"
+        >
+          Power Supply Unit
+        </Link>
+      </div>
+    ),
   },
   {
     key: "5",
-    label: <Link href="/">Storage Device</Link>,
+    label: (
+      <div className="hover:bg-primary transition-all rounded p-2 w-full">
+        <Link
+          href="/products/storage-device"
+          className="text-black hover:text-white"
+        >
+          Storage Device
+        </Link>
+      </div>
+    ),
   },
   {
     key: "6",
-    label: <Link href="/">Monitor</Link>,
+    label: (
+      <div className="hover:bg-primary transition-all rounded p-2 w-full">
+        <Link href="/products/monitors" className="text-black hover:text-white">
+          Monitor
+        </Link>
+      </div>
+    ),
   },
   {
     key: "7",
-    label: <Link href="/">Others</Link>,
-    danger: true,
+    label: (
+      <div className="hover:bg-primary transition-all rounded p-2 w-full">
+        <Link href="/products/others" className="text-black hover:text-white">
+          Others
+        </Link>
+      </div>
+    ),
   },
 ];
 
