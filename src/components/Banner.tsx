e4b46ac7ce3e0dfd634d7a5bea23c751a0c9a3c7
codewithshamim <shamimislamonline@gmail.com>
2023-08-15 20:58:36 +0600

    updated

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -5,24 +5,25 @@ import React from "react";
 const bannerItems = [
   {
     id: 1,
-    image: "/banner/banner1.png",
-  },
-  {
-    id: 2,
     image: "/banner/banner2.jpg",
   },
   {
-    id: 3,
-    image: "/banner/banner3.jpg",
+    id: 2,
+    image: "/banner/banner1.png",
   },
-  {
-    id: 4,
-    image: "/banner/banner4.jpg",
-  },
-  {
-    id: 4,
-    image: "/banner/banner5.jpg",
-  },
+
+  // {
+  //   id: 3,
+  //   image: "/banner/banner3.jpg",
+  // },
+  // {
+  //   id: 4,
+  //   image: "/banner/banner4.jpg",
+  // },
+  // {
+  //   id: 3,
+  //   image: "/banner/banner5.jpg",
+  // },
 ];
 
 const Banner = () => {
